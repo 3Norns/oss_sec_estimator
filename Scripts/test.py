3e6cbda99ce854f1ba83b9def712657ea14b290f
zywang <2123156731@qq.com>
2022-03-16 18:54:13 +0800

    2022.3.16

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,6 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
-url = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=alanaktion/phproject"
+url = "https://cve.mitre.org/cgi-bin/cvekey.cgi?keyword=bookstackapp%2Fbookstack"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
@@ -9,4 +11,8 @@ headers = {
 response = requests.get(url=url, headers=headers)
 html = response.text
 
-print(html)
+bs = BeautifulSoup(html, "html.parser")
+cve_number_regex = re.compile("CVE-[0-9]{4}-[0-9]{4,}")
+
+cve_numbers = bs.find_all(text=cve_number_regex)
+print(cve_numbers)
